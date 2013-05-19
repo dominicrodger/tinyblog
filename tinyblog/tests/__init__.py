37e5b0bf79dd2dfc86df6b5b414f14bd8caa62d0
Dominic Rodger <dominicrodger+github@gmail.com>
2013-05-19 17:37:41 +0100

    Tests for the JSON views

--- tinyblog/tests/__init__.py
+++ tinyblog/tests/__init__.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from django.test import TestCase
+from django.core import serializers
 from django.core.urlresolvers import reverse
 from tinyblog.models import Post
 import factory
@@ -13,7 +14,7 @@ class PostFactory(factory.Factory):
     created = datetime(2013, 1, 1, 7, 0, 0)
 
 
-class SimpleTest(TestCase):
+class TestBasicViews(TestCase):
     def test_index_with_no_entries(self):
         url = reverse('tinyblog_index')
         response = self.client.get(url)
@@ -56,3 +57,24 @@ class SimpleTest(TestCase):
         url = reverse('tinyblog_month', args=['2013', '01'])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
+    def test_get_post_json(self):
+        post1 = PostFactory.create()
+        post2 = PostFactory.create()
+        post3 = PostFactory.create()
+        later = datetime.now() + timedelta(days=1)
+        post4 = PostFactory.create(created=later)
+
+        url = reverse('tinyblog_json')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        posts = [p.object.title
+                 for p in serializers.deserialize("json", response.content)]
+
+        self.assertEqual(len(posts), 3)
+
+        self.assertTrue(post1.title in posts)
+        self.assertTrue(post2.title in posts)
+        self.assertTrue(post3.title in posts)
+        self.assertFalse(post4.title in posts)
