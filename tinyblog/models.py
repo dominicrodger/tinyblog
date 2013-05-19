a1e5c149adb16fae623dcc646333e63286a650f5
Dominic Rodger <dominicrodger+github@gmail.com>
2013-05-19 20:16:58 +0100

    Fixing PEP8

--- tinyblog/models.py
+++ tinyblog/models.py
@@ -33,18 +33,21 @@ class EmailSubscriber(models.Model):
 
     def confirm_url(self):
         current_site = Site.objects.get_current()
-        relative_url = reverse('tinyblog_subscribe_confirm', args=[self.uuid_second, ])
+        relative_url = reverse('tinyblog_subscribe_confirm',
+                               args=[self.uuid_second, ])
         return u'http://{0}{1}'.format(current_site, relative_url)
 
     def unsubscribe_url(self):
         current_site = Site.objects.get_current()
-        relative_url = reverse('tinyblog_unsubscribe', args=[self.uuid_second, ])
+        relative_url = reverse('tinyblog_unsubscribe',
+                               args=[self.uuid_second, ])
         return u'http://{0}{1}'.format(current_site, relative_url)
 
 
 class PublishedPostManager(models.Manager):
     def get_query_set(self):
-        return super(PublishedPostManager, self).get_query_set().filter(created__lte=datetime.now())
+        return super(PublishedPostManager,
+                     self).get_query_set().filter(created__lte=datetime.now())
 
 
 class Post(models.Model):
@@ -71,7 +74,8 @@ class Post(models.Model):
     def clean(self):
         # Don't allow draft entries to have a pub_date.
         if slugify(self.slug) != self.slug:
-            raise ValidationError('Slugs must only contain lowercase characters, numbers and hyphens.')
+            raise ValidationError('Slugs must only contain lowercase '
+                                  'characters, numbers and hyphens.')
 
     def get_teaser(self):
         if self.teaser_html:
