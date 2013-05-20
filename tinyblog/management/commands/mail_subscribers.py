44a9bd9e2b694632a412ca6cf95991e44aa299ad
Dominic Rodger <dominicrodger+github@gmail.com>
2013-05-20 17:12:11 +0100

    PEP 8

--- tinyblog/management/commands/mail_subscribers.py
+++ tinyblog/management/commands/mail_subscribers.py
@@ -9,16 +9,21 @@ from tinyblog.utils import get_from_email
 
 class Command(BaseCommand):
     args = 'url'
-    help = u'Sends the oldest unsent, published blog entry to all e-mail subscribers.'
+    help = ('Sends the oldest unsent, published blog entry to '
+            'all e-mail subscribers.')
 
     def generate_mail(self, post, subscriber):
         site = Site.objects.get_current()
 
         subject = post.title
         text_content = render_to_string('tinyblog/emails/blog_post.txt',
-                                        {'user': subscriber, 'site': site, 'post': post})
+                                        {'user': subscriber,
+                                         'site': site,
+                                         'post': post})
         html_content = render_to_string('tinyblog/emails/blog_post.html',
-                                        {'user': subscriber, 'site': site, 'post': post})
+                                        {'user': subscriber,
+                                         'site': site,
+                                         'post': post})
         to = subscriber.email
         msg = EmailMultiAlternatives(subject, text_content,
                                      get_from_email(), [to, ])
@@ -27,7 +32,8 @@ class Command(BaseCommand):
         return msg
 
     def handle(self, *args, **options):
-        posts = Post.published_objects.order_by('created').filter(emailed=False).all()
+        posts = Post.published_objects.order_by('created')
+        posts = posts.filter(emailed=False).all()
 
         if not posts:
             print "There are no posts to publish."
@@ -51,4 +57,6 @@ class Command(BaseCommand):
 
         earliest_post.emailed = True
         earliest_post.save()
-        print "'{0}' e-mailed to {1} subscriber(s).".format(earliest_post.title, len(subscribers))
+        print ("'{0}' e-mailed to {1} "
+               "subscriber(s).".format(earliest_post.title,
+                                       len(subscribers)))
