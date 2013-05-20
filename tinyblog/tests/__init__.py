a0ff98b450119be28501d451993f11fabc047dd6
Dominic Rodger <dominicrodger+github@gmail.com>
2013-05-20 17:10:03 +0100

    Very hacky test for mail_subscribers management command
    Now I have tests, I can refactor it

--- tinyblog/tests/__init__.py
+++ tinyblog/tests/__init__.py
@@ -2,3 +2,4 @@ from .test_basic_views import TestBasicViews
 from .test_subscribe_views import TestSubscribeViews
 from .test_posts import TestPosts
 from .test_feeds import TestFeeds
+from .test_mail_subscribers import TestMailSubscribers
---
+++ tinyblog/tests/test_mail_subscribers.py
@@ -0,0 +1,84 @@
+from django.core import mail
+from django.core.management import call_command
+from django.test import TestCase
+from StringIO import StringIO
+import sys
+
+from .utils import PostFactory, EmailSubscriberFactory
+
+
+def mail_subscribers():
+    content = StringIO()
+    original_stdout = sys.stdout
+    sys.stdout = content
+    call_command("mail_subscribers", stdout=content)
+    content.seek(0)
+    sys.stdout = original_stdout
+    return content.read().strip()
+
+
+class TestMailSubscribers(TestCase):
+    def test_no_posts(self):
+        self.assertEqual(mail_subscribers(),
+                         "There are no posts to publish.")
+
+    def test_no_subscribers(self):
+        post = PostFactory.create(title='Sample Post')
+        lines = mail_subscribers().split('\n')
+        self.assertEqual(len(lines), 2)
+        self.assertEqual(lines[0],
+                         "Sample Post")
+        self.assertEqual(lines[1],
+                         "'Sample Post' e-mailed to 0 subscriber(s).")
+
+    def test_with_subscribers(self):
+        post = PostFactory.create(title='Mail Everyone')
+
+        subscriber1 = EmailSubscriberFactory.create(
+            email='to1@example.com',
+            confirmed=True,
+        )
+
+        subscriber2 = EmailSubscriberFactory.create(
+            email='to2@example.com',
+            confirmed=True,
+        )
+
+        EmailSubscriberFactory.create(
+            email='not_subscribed@example.com',
+            confirmed=False,
+        )
+
+        EmailSubscriberFactory.create(
+            email='unsubscribed@example.com',
+            confirmed=True,
+            unsubscribed=True,
+        )
+
+        lines = mail_subscribers().split('\n')
+        self.assertEqual(len(lines), 2)
+        self.assertEqual(lines[0],
+                         "Mail Everyone")
+        self.assertEqual(lines[1],
+                         "'Mail Everyone' e-mailed to 2 subscriber(s).")
+
+        self.assertEqual(len(mail.outbox), 2)
+
+        self.assertEqual(mail.outbox[0].subject,
+                         'Mail Everyone')
+        self.assertEqual(mail.outbox[1].subject,
+                         'Mail Everyone')
+
+        self.assertEqual(mail.outbox[0].to,
+                         ['to1@example.com', ])
+        self.assertEqual(mail.outbox[1].to,
+                         ['to2@example.com', ])
+        self.assertEqual(mail.outbox[0].from_email,
+                         'from@example.com')
+        self.assertEqual(mail.outbox[1].from_email,
+                         'from@example.com')
+
+        unsubscribe_url1 = subscriber1.unsubscribe_url()
+        unsubscribe_url2 = subscriber2.unsubscribe_url()
+        self.assertTrue(mail.outbox[0].body.index(unsubscribe_url1) > 0)
+        self.assertTrue(mail.outbox[1].body.index(unsubscribe_url2) > 0)
