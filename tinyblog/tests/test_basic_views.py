9954aabd594014dbbdcb246497137f4a57366154
Dominic Rodger <dominicrodger+github@gmail.com>
2013-06-17 08:12:53 +0100

    Check that comments are hidden if no Disqus shortname is set

--- tinyblog/tests/test_basic_views.py
+++ tinyblog/tests/test_basic_views.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from django.test import TestCase
 from django.core import serializers
 from django.core.urlresolvers import reverse
+from django.test.utils import override_settings
 from .utils import PostFactory
 
 
@@ -23,6 +24,17 @@ class TestBasicViews(TestCase):
         self.assertNotContains(response, 'foo')
         self.assertContains(response, 'Hello')
         self.assertNotContains(response, 'World')
+        self.assertNotContains(response, 'disqus')
+
+    @override_settings(TINYBLOG_DISQUS_SHORTNAME='foobar')
+    def test_index_with_entries_and_disqus(self):
+        PostFactory.create(
+            teaser_html='<a class="foo">Hello</a>!',
+            text_html='<span class="western">World</span>.'
+        )
+        url = reverse('tinyblog_index')
+        response = self.client.get(url)
+        self.assertContains(response, 'disqus')
 
     def test_post_detail(self):
         post = PostFactory.create(
@@ -35,6 +47,16 @@ class TestBasicViews(TestCase):
         self.assertNotContains(response, 'foo')
         self.assertContains(response, 'Hello')
         self.assertContains(response, 'World')
+        self.assertNotContains(response, 'disqus')
+
+    @override_settings(TINYBLOG_DISQUS_SHORTNAME='foobar')
+    def test_post_detail_with_disqus(self):
+        post = PostFactory.create(
+            teaser_html='<a class="foo">Hello</a>!',
+            text_html='<span class="western">World</span>.'
+        )
+        response = self.client.get(post.get_absolute_url())
+        self.assertContains(response, 'disqus')
 
     def test_uncreated_post_detail(self):
         post = PostFactory.create(created=datetime.now() + timedelta(days=1))
