51d651013496670b8fb4893b72acc92ab0451220
Dominic Rodger <dominicrodger+github@gmail.com>
2015-01-13 08:22:24 +0000

    Switch some tests to normal py.test style

--- tests/test_bleach.py
+++ tests/test_bleach.py
@@ -1,55 +1,52 @@
-from django.test import TestCase
 from django.test.utils import override_settings
 from tinyblog.utils import tinyblog_bleach
 
 
-class TestBleach(TestCase):
-    def test_notags(self):
-        self.assertEqual(tinyblog_bleach('foobar'),
-                         'foobar')
+def test_notags():
+    assert tinyblog_bleach('foobar') == 'foobar'
 
-    def test_no_classes(self):
-        self.assertEqual(tinyblog_bleach('<strong class="western">'
-                                         'foo'
-                                         '</strong>'),
-                         '<strong>foo</strong>')
 
-    def test_links_work(self):
-        self.assertEqual(tinyblog_bleach('<a class="cat" href="bar">'
-                                         'foo'
-                                         '</a>'),
-                         '<a href="bar">foo</a>')
+def test_no_classes():
+    assert tinyblog_bleach(
+        '<strong class="western">foo</strong>'
+    ) == '<strong>foo</strong>'
 
-    def test_paragraphs_allowed(self):
-        self.assertEqual(tinyblog_bleach('<p class="western">'
-                                         'foo'
-                                         '</p>'),
-                         '<p>foo</p>')
 
-    @override_settings(TINYBLOG_NO_BLEACH=True)
-    def test_no_bleach_if_disabled(self):
-        self.assertEqual(tinyblog_bleach('<div class="western">'
-                                         'bar'
-                                         '</div>'),
-                         '<div class="western">bar</div>')
+def test_links_work():
+    assert tinyblog_bleach(
+        '<a class="cat" href="bar">foo</a>'
+    ) == '<a href="bar">foo</a>'
 
-    @override_settings(TINYBLOG_NO_BLEACH=False)
-    def test_bleach_if_enabled(self):
-        self.assertEqual(tinyblog_bleach('<div class="western">'
-                                         'bar'
-                                         '</div>'),
-                         'bar')
 
-    @override_settings(TINYBLOG_ALLOWED_TAGS=['div', ])
-    def test_overriding_allowed_tags(self):
-        self.assertEqual(tinyblog_bleach('<div class="western">'
-                                         'bar'
-                                         '</div>'),
-                         '<div>bar</div>')
+def test_paragraphs_allowed():
+    assert tinyblog_bleach(
+        '<p class="western">foo</p>'
+    ) == '<p>foo</p>'
 
-    @override_settings(TINYBLOG_ALLOWED_ATTRIBUTES={'p': ['class', ]})
-    def test_overriding_allowed_attributes(self):
-        self.assertEqual(tinyblog_bleach('<p class="western">'
-                                         'bar'
-                                         '</p>'),
-                         '<p class="western">bar</p>')
+
+@override_settings(TINYBLOG_NO_BLEACH=True)
+def test_no_bleach_if_disabled():
+    assert tinyblog_bleach(
+        '<div class="western">bar</div>'
+    ) == '<div class="western">bar</div>'
+
+
+@override_settings(TINYBLOG_NO_BLEACH=False)
+def test_bleach_if_enabled():
+    assert tinyblog_bleach(
+        '<div class="western">bar</div>'
+    ) == 'bar'
+
+
+@override_settings(TINYBLOG_ALLOWED_TAGS=['div', ])
+def test_overriding_allowed_tags():
+    assert tinyblog_bleach(
+        '<div class="western">bar</div>'
+    ) == '<div>bar</div>'
+
+
+@override_settings(TINYBLOG_ALLOWED_ATTRIBUTES={'p': ['class', ]})
+def test_overriding_allowed_attributes():
+    assert tinyblog_bleach(
+        '<p class="western">bar</p>'
+    ) == '<p class="western">bar</p>'
