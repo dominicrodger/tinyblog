725f971ed160ac2fc1ebadcc0991fba3e5d1516f
Dominic Rodger <dominicrodger+github@gmail.com>
2015-01-19 08:16:45 +0000

    Switch to CBV for subscribing

--- tinyblog/views/subscription.py
+++ tinyblog/views/subscription.py
@@ -2,30 +2,22 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from django.views.generic import DetailView
+from django.views.generic import DetailView, FormView
 from tinyblog.forms import EmailSubscriptionForm, EmailSubscriber
 from tinyblog.utils.mail import send_subscription_confirmation
 
 
-def subscribe(request):
-    if request.method == 'GET':
-        form = EmailSubscriptionForm()
-        return render_to_response('tinyblog/subscribe.html',
-                                  {'form': form},
-                                  context_instance=RequestContext(request))
-    else:
-        form = EmailSubscriptionForm(request.POST)
+class SubscriptionView(FormView):
+    form_class = EmailSubscriptionForm
+    template_name = 'tinyblog/subscribe.html'
 
-        if form.is_valid():
-            model = form.save()
-            request.session['tinyblog_thanks_uuid'] = str(model.uuid_first)
+    def form_valid(self, form):
+        model = form.save()
+        self.request.session['tinyblog_thanks_uuid'] = str(model.uuid_first)
 
-            send_subscription_confirmation(model)
-            return HttpResponseRedirect(reverse('tinyblog_subscribe_thanks'))
-
-        return render_to_response('tinyblog/subscribe.html',
-                                  {'form': form},
-                                  context_instance=RequestContext(request))
+        send_subscription_confirmation(model)
+        return HttpResponseRedirect(reverse('tinyblog_subscribe_thanks'))
+subscribe = SubscriptionView.as_view()
 
 
 class TinyBlogAcknowledgeSubscriptionView(DetailView):
